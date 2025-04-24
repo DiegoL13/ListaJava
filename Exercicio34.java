6a11ecd5bbfbb379a4ee24b1d58c6e86c57fd989
Diego Luiz <166581174+DiegoL13@users.noreply.github.com>
2025-04-24 11:17:31 -0300

    Create Exercicio34.java

---
+++ Exercicio34.java
@@ -0,0 +1,37 @@
+/*
+34) Escreva um programa que leia um conjunto de números positivos e exiba o menor e o maior. Suporemos que o 
+número de elementos deste conjunto não é conhecido, e que um número negativo será utilizado para sinalizar o fim 
+dos dados.
+*/
+
+import java.util.Scanner;
+
+public class Exercicio34 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int num,maior,menor;
+
+        System.out.print("Digite um número positivo ou digite um número negativo para sair: ");
+        num = entrada.nextInt();
+
+        if(num >=0){
+            menor = num;
+            maior = num;
+        while(num >=0) {
+            System.out.print("Digite outro número: ");
+            num = entrada.nextInt();
+            if (num > maior) {
+                maior = num;
+            }
+
+            if (num < menor) {
+                if (num >= 0)
+                    menor = num;
+                }
+            }
+            System.out.printf("O maior número é: %d e o menor é: %d",maior,menor);
+        }
+    }
+}
