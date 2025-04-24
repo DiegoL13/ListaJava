e87bca210b425b27324633574971f1961ce2ea9c
Diego Luiz <166581174+DiegoL13@users.noreply.github.com>
2025-04-24 11:09:06 -0300

    Create Exercicio29.java

---
+++ Exercicio29.java
@@ -0,0 +1,12 @@
+/*
+29) Faça um programa que exiba os números de 1 até 2000.
+*/
+
+public class Exercicio29 {
+    public static void main(String[] args) {
+        int num;
+        for(num=1;num<=2000;num++){
+            System.out.println(num);
+        }
+    }
+}
