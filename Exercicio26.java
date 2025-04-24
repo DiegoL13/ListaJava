50ecf16c85ab92aa40e2fe9b8f4a3fd87d36aa56
Diego Luiz <166581174+DiegoL13@users.noreply.github.com>
2025-04-24 11:01:55 -0300

    Create Exercicio26.java

---
+++ Exercicio26.java
@@ -0,0 +1,12 @@
+/*
+26) Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.
+*/
+
+public class Exercicio26 {
+    public static void main(String[] args) {
+        int num;
+        for(num=100;num>=1;num--){
+            System.out.println(num);
+        }
+    }
+}
