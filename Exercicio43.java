b9e9a344d085e8cc27cb796d8b85d53271aa5020
Diego Luiz <166581174+DiegoL13@users.noreply.github.com>
2025-04-26 08:49:18 -0300

    Update Exercicio43.java

--- Exercicio43.java
+++ Exercicio43.java
@@ -1,8 +1,23 @@
 /*
-  43) Uma rainha requisitou os serviços de um monge e disse-lhe que pagaria qualquer preço. O monge, necessitando de 
-alimentos, indagou à rainha sobre o pagamento, se poderia ser feito com grãos de trigo dispostos em um tabuleiro 
-de xadrez, de tal forma que o primeiro quadro deveria conter apenas um grão e os quadros subseqüentes, o dobro 
-do quadro anterior. A rainha achou o trabalho barato e pediu que o serviço fosse executado, sem se dar conta de 
-que seria impossível efetuar o pagamento. Faça um programa para calcular o número de grãos que o monge 
+  43) Uma rainha requisitou os serviços de um monge e disse-lhe que pagaria qualquer preço. O monge, necessitando de
+alimentos, indagou à rainha sobre o pagamento, se poderia ser feito com grãos de trigo dispostos em um tabuleiro
+de xadrez, de tal forma que o primeiro quadro deveria conter apenas um grão e os quadros subseqüentes, o dobro
+do quadro anterior. A rainha achou o trabalho barato e pediu que o serviço fosse executado, sem se dar conta de
+que seria impossível efetuar o pagamento. Faça um programa para calcular o número de grãos que o monge
 esperava receber.
 */
+
+public class Exercicio43 {
+    public static void main(String[] args) {
+        long graos=1;
+        for(int i=1;i<=64;i++){
+            if(i>=2){
+                graos*=2;
+                System.out.printf("Casa: %d - grãos: %d\n",i,graos);
+            }
+            else{
+                System.out.printf("Casa: %d - grãos: %d\n",i,graos);
+            }
+        }
+    }
+}
